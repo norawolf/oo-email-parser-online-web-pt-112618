4874d791fcb968f8d9bec4aa0907cac318c0f6b5
Nora Vogt <noraevogt@gmail.com>
2018-12-23 02:40:59 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,6 +11,9 @@ class EmailParser
   end
 
   def parse
-    @emails.split(/[,\s]/).uniq.reject {|e| e.empty?}
+    #refactoring
+    @emails.split(/, | /).uniq
+    # original
+    # @emails.split(/[,\s]/).uniq.reject {|e| e.empty?}
   end
 end
